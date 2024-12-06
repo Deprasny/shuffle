c58086bd5cc1d7cc0e6aab9fca100238d7596d99
Depras Nur Yadi <deprasny@gmail.com>
2024-12-06 14:00:16 +0700

    chore: update vite config and add eslint configuration

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/shuffle/" : "/",
+  base: mode === "production" ? "/shuffle/" : "/",
   publicDir: "public",
   server: {
     port: 5173,
     open: true
   }
-});
+}));
